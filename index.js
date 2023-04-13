005d51259a2d233cddbca4b1be81eb89e008e127
douglas-cavalcante <douglascavalcante2803@gmail.com.br>
2023-04-13 20:03:10 -0300

    adiciona exemplo de query params

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const conexao = require('./src/database')
 const Pet = require('./src/models/pet')
 
 const createPet = require('./src/controllers/pets/createPet')
+const findAllPet = require('./src/controllers/pets/findAllPet')
 
 const app = express()
 app.use(express.json()) // obrigatório
 
 app.post('/api/pets', createPet )
+app.get('/api/pets', findAllPet )
 
 
 conexao.sync({alter: true})
